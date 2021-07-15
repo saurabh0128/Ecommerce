a6f33ead71348c5ca7313fa2a0deca531b65dbe1
bhargav kubavat <bhargav@hopeleaftechnologies.com>
2021-07-15 17:04:22 +0530

    create backend folder test

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -14,7 +14,7 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return view('admin.test');
+        return view('backend.admin.test');
     }
 
     /**
