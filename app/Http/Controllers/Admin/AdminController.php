063197f25d71a0ecdd403914a34858f65d285bc3
bhargav kubavat <bhargav@hopeleaftechnologies.com>
2021-07-15 17:06:52 +0530

    test2

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -14,7 +14,7 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return view('backend.admin.index');
+        return view('backend/admin/admin.index');
     }
 
     /**
