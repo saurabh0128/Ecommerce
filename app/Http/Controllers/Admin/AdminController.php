724888f5408a62ef6baf3361489c74517eb0372a
bhargav kubavat <bhargav@hopeleaftechnologies.com>
2021-07-15 16:48:42 +0530

    change folder name

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -14,7 +14,7 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return view('Admin.Admin.index');
+        return view('admin.admin.index');
     }
 
     /**
