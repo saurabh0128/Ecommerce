259873312c5d954f63d6c7c3dee367c6b23eb3f1
bhargav kubavat <bhargav@hopeleaftechnologies.com>
2021-07-15 18:11:52 +0530

    change disgin in login file

--- resources/views/backend/admin/index.blade.php
+++ resources/views/backend/admin/index.blade.php
@@ -39,9 +39,9 @@
                         <div class="row">
                             <div class="col-md-10 offset-md-1">
                                 <div class="d-block d-lg-none text-center text-lg-start">
-                                    <img width="120" src="https://vetra.laborasyon.com/assets/images/logo.svg " alt="logo">
                                 </div>
                                 <div class="my-5 text-center text-lg-start">
+                                    <img width="120" src="https://vetra.laborasyon.com/assets/images/logo.svg " alt="logo">
                                     <h1 class="display-8">Sign In</h1>
                                     <p class="text-muted">Sign in to Vetra to continue</p>
                                 </div>
@@ -59,14 +59,6 @@
                                         <button class="btn btn-primary">Sign In</button>
                                     </div>
                                 </form>
-                                <div class="social-links justify-content-center">
-                                    <a href="#">
-                                        <i class="ti-google bg-google"></i> Sign in with Google
-                                    </a>
-                                    <a href="#">
-                                        <i class="ti-facebook bg-facebook"></i> Sign in with Facebook
-                                    </a>
-                                </div>
                                 <p class="text-center d-block d-lg-none mt-5 mt-lg-0">
                                     Don't have an account? <a href="#">Sign up</a>.
                                 </p>
@@ -74,14 +66,7 @@
                         </div>
                     </div>
                     <div class="col d-none d-lg-flex border-start align-items-center justify-content-between flex-column text-center">
-                        <div class="logo">
-                            <img width="120" src="https://vetra.laborasyon.com/assets/images/logo.svg " alt="logo">
-                        </div>
-                        <div>
-                            <h3 class="fw-bold">Welcome to Vetra!</h3>
-                            <p class="lead my-5">If you don't have an account, would you like to register right now?</p>
-                            <a href="#" class="btn btn-primary">Sign Up</a>
-                        </div>
+                        
                         <ul class="list-inline">
                             <li class="list-inline-item">
                                 <a href="#">Privacy Policy</a>
