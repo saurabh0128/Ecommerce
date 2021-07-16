a5fd002ab578a74762f5c845bea4615eb29659db
saurabh0128 <saurabh@hopeleaftechnologies.com>
2021-07-16 10:51:59 +0530

    table completed to puchase items

--- database/migrations/2021_07_16_045309_create_coupons_table.php
+++ database/migrations/2021_07_16_045309_create_coupons_table.php
@@ -15,6 +15,14 @@ class CreateCouponsTable extends Migration
     {
         Schema::create('coupons', function (Blueprint $table) {
             $table->id();
+            $table->string('coupon_code',40);
+            $table->text('coupon_details');
+            $table->decimal('coupon_discount',8,2);
+            $table->string('coupon_type',20);
+            $table->string('dicount_type',20);
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('total_uses');
             $table->timestamps();
         });
     }
