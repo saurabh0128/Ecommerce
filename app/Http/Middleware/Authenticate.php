048719191160141df43795289666633517784546
saurabh0128 <saurabh@hopeleaftechnologies.com>
2021-07-19 11:05:04 +0530

    completed up to a middleware
    authentication middleware completed.
    all layout file like header , footer ,file completed.

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,22 +2,45 @@
 
 namespace App\Http\Middleware;
 
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
+use Illuminate\Support\Facades\Auth;
+
+use Closure;
+use Illuminate\Http\Request;
 
 
-class Authenticate extends Middleware
+class Authenticate //extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
+     * 
+     * 
      */
+
+    public function handle(Request $request, Closure $next)
+    {
+        $id = Auth::id();
+
+       if($id)
+       { 
+            return $next($request);
+       }
+       else {
+            throw new HttpException(404);
+        } 
+    }
+
+
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            return route('admin.login.index');
         }
     }
    
---
+++ resources/views/backend/layout/setting_sidebar.blade.php
@@ -0,0 +1,68 @@
+<!-- settings sidebar -->
+<div class="sidebar" id="settings">
+    <div class="sidebar-header">
+        <div>
+            <i class="bi bi-gear me-2"></i>
+            Settings
+        </div>
+        <button data-sidebar-close>
+            <i class="bi bi-arrow-right"></i>
+        </button>
+    </div>
+    <div class="sidebar-content">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item px-0 border-0">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1" checked>
+                    <label class="form-check-label" for="flexCheckDefault1">
+                        Remember next visits
+                    </label>
+                </div>
+            </li>
+            <li class="list-group-item px-0 border-0">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault2" checked>
+                    <label class="form-check-label" for="flexCheckDefault2">
+                        Enable report generation.
+                    </label>
+                </div>
+            </li>
+            <li class="list-group-item px-0 border-0">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault3" checked>
+                    <label class="form-check-label" for="flexCheckDefault3">
+                        Allow notifications.
+                    </label>
+                </div>
+            </li>
+            <li class="list-group-item px-0 border-0">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault4">
+                    <label class="form-check-label" for="flexCheckDefault4">
+                        Hide user requests
+                    </label>
+                </div>
+            </li>
+            <li class="list-group-item px-0 border-0">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault5" checked>
+                    <label class="form-check-label" for="flexCheckDefault5">
+                        Speed up demands
+                    </label>
+                </div>
+            </li>
+            <li class="list-group-item px-0 border-0">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                        Hide menus
+                    </label>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="sidebar-action">
+        <a href="#" class="btn btn-primary">All Settings</a>
+    </div>
+</div>
+<!-- ./ settings sidebar -->
