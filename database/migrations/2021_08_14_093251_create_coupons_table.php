6f12ce41be37a8097e135540644d3a6ff78402ff
bhargav kubavat <bhargav@hopeleaftechnologies.com>
2021-08-14 18:09:55 +0530

    api coupon
     - change cord in cart - [Done]
    - create Api for coupon - [Done]
    - Create insert for coupon - [In Process]

--- database/migrations/2021_08_14_093251_create_coupons_table.php
+++ database/migrations/2021_08_14_093251_create_coupons_table.php
@@ -15,14 +15,17 @@ class CreateCouponsTable extends Migration
     {
         Schema::create('coupons', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->nullable();
             $table->string('coupon_code',40);
             $table->text('coupon_details');
             $table->decimal('coupon_discount',8,2);
             $table->string('coupon_type',20);
-            $table->string('dicount_type',20);
+            $table->string('discount_type',20);
+            $table->unsignedInteger('total_uses')->nullable()->change();
+            $table->integer('coupon_type_value');
             $table->date('start_date');
             $table->date('end_date');
-            $table->integer('total_uses');
+            
             $table->timestamps();
         });
     }
